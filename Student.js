cf4f74406836e215a50d3aeef00c817f095f2114
Uma Mahesh <uma.kondapally@gmail.com>
2018-02-06 09:04:51 +0530

    added super keyword and extends

--- Student.js
+++ Student.js
@@ -15,9 +15,16 @@ class Student {
     set firstName1(name) {
         this.firstName = name;
     }
+    static upperCaseFullName() {
+        this.firstName="firstName";
+        this.lastName="lastName";
+        let name = `${this.firstName} ${this.lastName}`;
+        return `${name.toUpperCase()}`;
+    }
 }
 let stud = new Student('Uma Mahesh', 'Kondapalli');
 stud.fullName();//Uma Mahesh Kondapalli
 console.log(stud.firstName1);//Uma Mahesh
 stud.firstName1 = 'ABCD';
 console.log(stud.firstName1);//ABCD
+console.log(Student.upperCaseFullName());//FIRSTNAME LASTNAME
---
+++ Student1.js
@@ -0,0 +1,24 @@
+class Student {
+    constructor(name) {
+        this.name = name;
+    }
+    display() {
+        console.log(this.name);
+    }
+    display1() {
+        console.log(this.name + '  in Student');
+    }
+}
+class Employee extends Student {
+    display() {
+        console.log(this.name);
+    }
+    display1() {
+        super.display1();
+        console.log(this.name + '  in Employee');
+    }
+}
+let emp = new Employee('Uma Mahesh');
+emp.display();//Uma Mahesh
+emp.display1();//Uma Mahesh in in Student 
+                //Uma Mahesh in Employee
